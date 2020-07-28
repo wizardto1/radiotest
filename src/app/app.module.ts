d78e73cc00713bf5d8a365b508b3783922063d04
wizardto1 <ph429@drexel.edu>
2020-07-27 22:38:47 -0400

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo : '/page1', pathMatch:'full' },{path:'page1', component: Page1Component, pathMatch:'full' },{path:'page2', component: Page2Component, pathMatch:'full' }
+  { path: '', redirectTo : '/page2', pathMatch:'full' },{path:'page1', component: Page1Component, pathMatch:'full' },{path:'page2', component: Page2Component, pathMatch:'full' }
 ];
 
 @NgModule({
