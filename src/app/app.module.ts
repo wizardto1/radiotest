23f6c5a5f93b0567404d213d7104411bcc33c971
wizardto1 <ph429@drexel.edu>
2020-07-27 22:36:03 -0400

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo : '/page1', pathMatch:'full' },{path:'page1', component: Page1Component }
+  { path: '', redirectTo : '/page2', pathMatch:'full' },{path:'page1', component: Page1Component, pathMatch:'full' },{path:'page2', component: Page2Component, pathMatch:'full' }
 ];
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,  RouterModule.forRoot(appRoutes)  ],
-  declarations: [ AppComponent, HelloComponent, Page1Component ],
+  declarations: [ AppComponent, HelloComponent, Page1Component, Page2Component ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
