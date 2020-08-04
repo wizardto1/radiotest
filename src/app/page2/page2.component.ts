37ee074625256a49d5807db5873732a09d592798
wizardto1 <ph429@drexel.edu>
2020-08-04 18:20:53 -0400

    3 files modified

--- src/app/page2/page2.component.ts
+++ src/app/page2/page2.component.ts
@@ -7,8 +7,8 @@ import { ScoresService } from '../scores.service';
 })
 export class Page2Component implements OnInit {
   score1:number;
-  flag:any;
-
+  flag:any=null;
+  buttonDisabled:boolean=false;
   constructor(private scoresService: ScoresService) { this.flag = 0; }
   setScore2(flag){
     this.scoresService.setScore2(flag)
@@ -16,15 +16,21 @@ export class Page2Component implements OnInit {
 
 
   ngOnInit() {
-    if(this.flag) 
-    {this.flag=0;}
-    else{
+    
     this.flag=this.scoresService.getScore2()
-    }
+    console.log(this.flag)
+    /*--if(this.flag!==null) 
+    {this.buttonDisabled=true;}--*/
+    
+    
+    
   }
 
   radioEventHandler(event:any){
-    this.score1=event.target.value
+    this.score1=event.target.value;
+    
+    this.buttonDisabled=true;
+    
   }
 
 }
--- src/app/scores.service.ts
+++ src/app/scores.service.ts
@@ -15,9 +15,11 @@ export class ScoresService {
     console.log(this.score2);
   }
   getScore1(){
+
     return this.score1
   }
-  getScore2(){return this.score2}
+  getScore2(){console.log(this.score2);
+  return this.score2}
   getTotal(){
     this.total=parseInt(this.score1)+parseInt(this.score2); 
   return this.total;
